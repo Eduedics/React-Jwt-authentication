2bf5f9d5b54f2f5dbdd12238a6cb0e370926f715
Edward <kemuthii@gmail.com>
2025-05-14 11:05:14 +0300

    wrap private routes inside protected route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import PrivateRoute from './utils/PrivateRoute'
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
 
 function App() {
@@ -11,7 +12,9 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route element={<Home/>} path='/'/>
+          <PrivateRoute >
+            <Route element={<Home/>} path='/' />
+          </PrivateRoute>
           <Route element={<Login/>} path='/login'/>
         </Routes>
       </Router>
