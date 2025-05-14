68c34d8b94489e03ba20d60628658d1731205d3a
Edward <kemuthii@gmail.com>
2025-05-14 13:03:22 +0300

    hide links based on whether user is loged in or not

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,30 @@
-
 import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import AuthContext from '../context/AuthContext'
 
-
 export const Home = () => {
   const { user, logoutUser } = useContext(AuthContext)
+
   return (
     <div>
-        <Link to='/'>Home</Link>
-        <span>|</span>
-        <Link to='/login'>Login</Link>
-        <button onClick={logoutUser}>Logout</button>
-        <h1>Welcome, {user?.username}!</h1>
-      
+      <nav>
+        {user && (
+          <>
+            <Link to="/">Home</Link>
+            <span> | </span>
+            <button onClick={logoutUser}>Logout</button>
+            <span> | </span>
+            <span>Welcome, {user.username}!</span>
+          </>
+        )}
+        {!user && (
+          <Link to="/login">Login</Link>
+        )}
+      </nav>
+
+      <h1>Home Page</h1>
     </div>
   )
 }
-export default Home;
+
+export default Home
