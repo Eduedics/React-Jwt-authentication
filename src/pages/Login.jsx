f142a5d96fd34af05c520c0a58c2f982a20a0b3b
Edward <kemuthii@gmail.com>
2025-05-14 14:46:46 +0300

    add class for styling

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
-// src/pages/Login.js
 import React, { useState, useContext } from 'react'
 import AuthContext from '../context/AuthContext'
 import { Link } from 'react-router-dom'
+import './Login.css' // Make sure this path matches your structure
 
 const Login = () => {
   const [username, setUsername] = useState('')
@@ -15,23 +15,30 @@ const Login = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type="submit">Login</button>
-      <p>Don't have an account? <Link to="/register">Register here</Link></p>
-      <p><a href="/forgot-password">Forgot Password?</a></p>
-    </form>
+    <div className="login-container">
+      <form onSubmit={handleSubmit} className="login-form">
+        <div className="login-links">
+          <Link to="/register" className="login-link">Register</Link>
+          <a href="/forgot-password" className="login-emphasis">Forgot Password?</a>
+        </div>
+
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="login-input"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="login-input"
+        />
+        <button type="submit" className="login-button">Login</button>
+      </form>
+    </div>
   )
 }
 
